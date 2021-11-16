d5629e5165eafed75d6d7142e1fd037bcd55ef17
Amanda Zotelli <amzotelli@gmail.com>
2021-11-16 15:46:02 -0300

    tela de explorar

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Button from '../components/Button';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Explorar() {
   return (
-    <div>
-      Explorar
+    <div className="container-explore">
+      <Header />
+      <Link to="/explorar/comidas">
+        <Button
+          dataTestId="explore-food"
+          text="Explorar Comidas"
+        />
+      </Link>
+      <Link to="/explorar/bebidas">
+        <Button
+          dataTestId="explore-drinks"
+          text="Explorar Bebidas"
+        />
+      </Link>
+      <Footer />
     </div>
   );
 }
